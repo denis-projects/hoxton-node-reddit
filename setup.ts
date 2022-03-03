ac50cd67b9e452677e98deea8c71f66786d7b858
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-03 14:05:27 +0100

    first commit

---
+++ setup.ts
@@ -0,0 +1,125 @@
+import Database from "better-sqlite3";
+
+const db = new Database("./data.db", {
+    verbose: console.log,
+});
+
+db.exec(`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS subreddits;
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE users (
+    id INTEGER,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    displayName TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE subreddits (
+    id INTEGER,
+    description TEXT,
+    background TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE posts (
+    id INTEGER,
+    userId INTEGER,
+    subredditId INTEGER,
+    title TEXT,
+    content TEXT,
+    createdAt TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+    FOREIGN KEY (subredditId) REFERENCES subreddits(id) 
+);
+`);
+
+const users = [
+    {
+        name: "Denis",
+        email: "denis@email.com",
+        password: "denis",
+        displayName: "Den"
+    },
+    {
+        name: "Elon",
+        email: "elon.musk@email.com",
+        password: "tesla",
+        displayName: "Elon"
+    },
+    {
+        name: "Tim",
+        email: "tim.cook@email.com",
+        password: "apple",
+        displayName: "Tim"
+    },
+    {
+        name: "Warren",
+        email: "warren.buffet@email.com",
+        password: "cocacola",
+        displayName: "Warren"
+    },
+    {
+        name: "Bill",
+        email: "bill.gates@email.com",
+        password: "microsoft",
+        displayName: "Bill"
+    }
+]
+
+
+
+
+const subreddits = [
+    {
+        descripton: "description one",
+        background: "background one",
+    },
+    {
+        descripton: "description two",
+        background: "background two",
+    },
+    {
+        descripton: "description three",
+        background: "background three",
+    }
+]
+
+// const posts = [
+//     {
+//         title: "first post",
+//         content: "content of first post",
+//         createdAt: "01/01/2020"
+//     },
+//     {
+//         title: "second post",
+//         content: "content of second post",
+//         createdAt: "01/01/2020"
+//     },
+//     {
+//         title: "third post",
+//         content: "content of third post",
+//         createdAt: "01/01/2020"
+//     }
+
+// ]
+
+
+const createUser = db.prepare(`
+INSERT INTO users (name, email, password, displayName) VALUES (?, ?, ?, ?)
+`)
+
+for (const user of users) {
+    createUser.run(user.name, user.email, user.password, user.displayName)
+}
+
+
+const createSubreddit = db.prepare(`
+INSERT INTO subreddits (description, background) VALUES (?, ?)
+`)
+for (const subreddit of subreddits) {
+    createSubreddit.run(subreddit.descripton, subreddit.background)
+}

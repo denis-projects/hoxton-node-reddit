fe5851921fc8671fce9a5bb3929c9924d07e26de
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-03 14:18:50 +0100

    creating posts and subreddits on setup file

--- setup.ts
+++ setup.ts
@@ -5,9 +5,9 @@ const db = new Database("./data.db", {
 });
 
 db.exec(`
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS subreddits;
-DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
     id INTEGER,
@@ -88,24 +88,31 @@ const subreddits = [
     }
 ]
 
-// const posts = [
-//     {
-//         title: "first post",
-//         content: "content of first post",
-//         createdAt: "01/01/2020"
-//     },
-//     {
-//         title: "second post",
-//         content: "content of second post",
-//         createdAt: "01/01/2020"
-//     },
-//     {
-//         title: "third post",
-//         content: "content of third post",
-//         createdAt: "01/01/2020"
-//     }
 
-// ]
+const posts = [
+    {
+        userId: 1,
+        subredditId: 1,
+        title: "first post",
+        content: "content of first post",
+        createdAt: "01/01/2020"
+    },
+    {
+        userId: 2,
+        subredditId: 2,
+        title: "second post",
+        content: "content of second post",
+        createdAt: "01/01/2020"
+    },
+    {
+        userId: 3,
+        subredditId: 3,
+        title: "third post",
+        content: "content of third post",
+        createdAt: "01/01/2020"
+    }
+
+]
 
 
 const createUser = db.prepare(`
@@ -122,4 +129,12 @@ INSERT INTO subreddits (description, background) VALUES (?, ?)
 `)
 for (const subreddit of subreddits) {
     createSubreddit.run(subreddit.descripton, subreddit.background)
+}
+
+
+const createPost = db.prepare(`
+INSERT INTO posts (userId, subredditId, title, content, createdAt) VALUES (?, ?, ?, ?, ?)
+`)
+for (const post of posts) {
+    createPost.run(post.userId, post.subredditId, post.title, post.content, post.createdAt)
 }
